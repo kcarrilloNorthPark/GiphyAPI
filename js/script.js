dcd544de1db1100e26ada06cc01d10c5e163be11
Kevi5002 <93739889+Kevi5002@users.noreply.github.com>
2025-11-23 05:01:45 -0600

    Added api key and a few constants

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("script.js loaded");
+
+var endpoint = "https://api.giphy.com/v1/gifs/search?api_key=pUs7ayGnN3GewPPzch4HddPqY9t94OUD&q=mango&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+const gifContainer = document.querySelector("#gif-container");
+const fetchButton = document.querySelector("#fetch-gif-btn");
+const searchInput = document.querySelector("#gif-search");
+
+let images = [];
+
